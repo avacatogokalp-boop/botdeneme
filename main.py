423cd377e04ede57df4d69cdac5a96ef29aa1b0d
avacatogokalp-boop <avacatogokalp@gmail.com>
2026-04-13 16:52:46 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from datetime import datetime, timezone
 from flask import Flask, send_from_directory, request, jsonify
 
 TOKEN = os.environ.get("BOT_TOKEN")
-SITE_LINKI = "https://cutt.ly/deoKNC0g"
+SITE_LINKI = "https://cutt.ly/7tF5Ow3K"
 GIF_URL = "https://i.ibb.co/KcrpQfD4/denemeeeee.png"
 RENDER_URL = os.environ.get("RENDER_EXTERNAL_URL", "https://botdeneme.onrender.com")
 MINI_APP_URL = f"{RENDER_URL}/wheel"
