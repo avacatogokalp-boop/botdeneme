d4d97f345d5e01877567765c4adf5ce8d7d5da3a
avacatogokalp-boop <avacatogokalp@gmail.com>
2026-04-21 23:46:38 +0200

    Update main.py

--- main.py
+++ main.py
@@ -333,7 +333,7 @@ def api_use_spin():
         add_bonus_spin(user_id, 1)
 
     def delayed_message():
-        time.sleep(13.0)
+        time.sleep(15.5)
         try:
             if win and prize:
                 if prize == "+1 Freespin":
